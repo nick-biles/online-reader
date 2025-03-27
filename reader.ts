a0aaa9413e4a0628b3ab6dab891f88260cb945d3
nick-biles <bilesn@vcu.edu>
2025-03-26 21:16:09 -0400

    Reworked how events are attached to buttons.

--- reader.ts
+++ reader.ts
@@ -1,13 +1,16 @@
 // Define element that contains scroll speed
 let speedElement = document.getElementById("scrollspeed") as HTMLInputElement;
 
-
-function addEventListen(element: Element, action: string, request: String) {
+function addEventListen(element: Element, action: string, request: String, parameters) {
     element.addEventListener(action, function() {
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+            // Determine the active tab, the tab we want to start scrolling
             var activeTab = tabs[0];
-            let speed = document.getElementById("scrollspeed").value;
-            chrome.tabs.sendMessage(activeTab.id, { request: request, speed: speed})
+            
+            // Retrieve the speed
+            let speed = parameters[0].value;
+            let persist = parameters[1];
+            chrome.tabs.sendMessage(activeTab.id, { request: request, speed: speed, persist: persist})
             .then(() => {}, (err) => {
                 chrome.runtime.sendMessage({ for: "background", request: "injectActiveTab", to: activeTab});
                 setTimeout(chrome.tabs.sendMessage, 1000, activeTab.id, { request: request, speed: speed});
@@ -16,15 +19,20 @@ function addEventListen(element: Element, action: string, request: String) {
     })
 }
 
-const eventDict = {scrollbutton: {click: ["startScrolling"]}, autoscrollbutton: {click: ["autoScroll"]}, stopscrollbutton: {click: ["stopScrolling"]}};
+// Define what events are associated with each button
+const eventDict = [
+    {action: "click", elementID: "scrollbutton", func: "startScrolling", parameter: [speedElement, false]},
+    {action: "click", elementID: "autoscrollbutton", func: "startScrolling", parameter: [speedElement, true]},
+    {action: "click", elementID: "stopscrollbutton", func: "startScrolling", parameter: [{value: 0}, true]}
+];
+
+// Add event listeners when called
 function onPopup() {
     let elements = document.getElementById("menubar");
     let actionElements = document.getElementsByClassName("action");
-    for (const el of Array.from(actionElements)) {
-        const elementEvents = eventDict[el.id];
-        for (const key in elementEvents) {
-            addEventListen(el, key, elementEvents[key]);
-        }
+    for (const event of eventDict) {
+        const element = document.getElementById(event.elementID);
+        addEventListen(element, event.action, event.func, event.parameter);
     }
     chrome.storage.session.get("lastSpeed")
     .then((lastSpeed) => {
